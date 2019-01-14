2d91a784b106686ba4c962fef9a9919845d1fa5b
info@appertunity.net <info@appertunity.net>
2019-01-14 15:06:54 +0500

    s

--- application/views/common/contact.php
+++ application/views/common/contact.php
@@ -3,8 +3,8 @@
 
     <h2>Contact Us Page</h2>
     <div class="row">
-        <div class="col-md-8" style="height: 500px;">
-            <div class="well well-sm">
+        <div class="col-md-8" >
+            <div class="well well-sm" style="height: 500px;">
                 <form>
                     <div class="row">
                         <div class="col-md-6" style="float: right">
@@ -20,7 +20,7 @@
                                 <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
                                 </span>
                                     <input type="email" class="form-control" id="email" placeholder="البريد الإلكتروني
-" required="required"></div>
+" required="required" style="width: 100% !important;"></div>
                             </div>
                             <div class="form-group">
                                 <label for="subject">
