151c473dd69667007e807621d7b90c2f1dda8945
info@appertunity.net <info@appertunity.net>
2019-02-19 18:07:43 +0500

    Voice Gallery

--- application/views/common/voice-gallery.php
+++ application/views/common/voice-gallery.php
@@ -65,17 +65,16 @@
 
 <script>
     $(document).ready(function () {
+        var audio
         $('.sound-button-on').click(function(){
             voice = $(this).data('sound')
-            var audio = new Audio(voice)
+            audio = new Audio(voice)
             audio.play()
            $(this).html('<i class="fa fa-pause"></i>')
             $(this).addClass('sound-button-off').removeClass('sound-button-on')
 
 
             $('.sound-button-off').click(function(){
-                voice = $(this).data('sound')
-                var audio = new Audio(voice)
                 audio.pause()
                 $(this).html('<i class="fa fa-play"></i>')
                 $(this).addClass('sound-button-on').removeClass('sound-button-off')
