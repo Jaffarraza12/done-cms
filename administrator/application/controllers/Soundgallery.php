3c1beca27f646fdba64b7f1728a18bf569529621
info@appertunity.net <info@appertunity.net>
2019-02-21 12:33:19 +0500

    check up

---
+++ administrator/application/controllers/Soundgallery.php
@@ -0,0 +1,14 @@
+<?php
+
+class Soundgallery extends CI_Controller {
+
+    //Show all the news on 1 page..
+    public function View($data='')
+    {
+        //Load languages and Default Language
+       echo '212';
+    }
+    
+
+
+}
