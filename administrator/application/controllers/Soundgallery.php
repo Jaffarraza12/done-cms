f82e291e175e9f4431d31d9dab611c6ff7a9b4a9
info@appertunity.net <info@appertunity.net>
2019-02-21 12:52:44 +0500

    check up

--- administrator/application/controllers/Soundgallery.php
+++ administrator/application/controllers/Soundgallery.php
@@ -7,8 +7,23 @@ class Soundgallery extends CI_Controller {
     //Show all the news on 1 page..
     public function View($data='')
     {
-       echo 1 ;
-       exit();
+        //Load languages and Default Language
+        $data['Languages'] = $this->MUtils->getLanguages();
+        $data['defaultLang'] = $this->MUtils->getDefaultLanguage();
+
+        //BreadCrumb URLs
+        $data['breadcrumb_link1'] = "/Voice";
+        $data['breadcrumb_anchor1'] = "Voice";
+
+        $sql = "select * from voice_gallery order by id asc";
+        $page_content = $this->db->query($sql)->result();
+
+
+
+        $data['page_content'] = $page_content;
+        $data['main_content'] = "Admin/Voice/view";
+
+        $this->load->view('Admin/default.php', $data);
     }
 
 
