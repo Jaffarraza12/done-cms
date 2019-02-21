b843c660802496886797cdfda77853afc541d040
info@appertunity.net <info@appertunity.net>
2019-02-21 12:37:34 +0500

    check up

--- administrator/application/controllers/Soundgallery.php
+++ administrator/application/controllers/Soundgallery.php
@@ -25,5 +25,155 @@ class Soundgallery extends CI_Controller {
     }
 
 
+    public function Add()
+    {
+        //Load languages and Default Language
+        $data['Languages'] = $this->MUtils->getLanguages();
+        $data['defaultLang'] = $this->MUtils->getDefaultLanguage();
+
+        //BreadCrumb URLs
+        $data['breadcrumb_link1'] = "/Videos/View";
+        $data['breadcrumb_anchor1'] = "Videos";
+        $data['breadcrumb_link2'] = "/Videos/Add";
+        $data['breadcrumb_anchor2'] = "Add Videos";
+
+        $data['activeMenu'] = "mnuVideos";
+
+        $data['main_content'] = "Admin/Videos/add";
+        $this->load->view('Admin/default.php', $data);
+
+    }
+
+    //Show the view to edit news
+    public function Edit()
+    {
+        //Load languages and Default Language
+        $data['Languages'] = $this->MUtils->getLanguages();
+        $data['defaultLang'] = $this->MUtils->getDefaultLanguage();
+
+        //BreadCrumb URLs
+        $data['breadcrumb_link1'] = "/Videos/View";
+        $data['breadcrumb_anchor1'] = "Videos";
+        $data['breadcrumb_link2'] = "/Videos/Edit?id=" . (int)$this->input->get("id");
+        $data['breadcrumb_anchor2'] = "Edit Videos";
+
+        $data['activeMenu'] = "mnuVideos";
+
+        //Loading Data for this view
+        $this->load->model("MVideos");
+
+        $data['recId'] = (int)$this->input->get("id");
+        $data['record']= $this->MVideos->viewById($data['recId'])->row();
+
+        $data['main_content'] = "Admin/Videos/edit";
+        $this->load->view('Admin/default.php', $data);
+
+    }
+
+    public function AddVideos()
+    {
+        //Load languages and Default Language
+        $languages = $this->MUtils->getLanguages();
+        $data['Languages'] = $languages;
+        $data['defaultLang'] = $this->MUtils->getDefaultLanguage();
+
+        //BreadCrumb URLs
+        $data['breadcrumb_link1'] = "/Videos/Edit";
+        $data['breadcrumb_anchor1'] = "Videos";
+        $data['breadcrumb_link2'] = "/Videos/Add";
+        $data['breadcrumb_anchor2'] = "Add Videos";
+
+        $data['activeMenu'] = "mnuVideos";
+
+        // $data['smallImage'] = $this->MUtils->doUploadWithCropping('smallFile', 70, 92);
+        //$data['largeImage'] = $this->MUtils->doUpload('largeFile',0 ,0, false);
+        //$data['sliderImage'] = $this->MUtils->doUpload('sliderFile', 1920, 530, true);
+        $dat['title'] = $this->input->post('title');
+        $dat['link'] = $this->input->post('link');
+        $dat['status'] = $this->input->post('status');
+
+
+        $defaultCode = $languages[$data['defaultLang']]->code;
+        $status = "";
+        if ( isset($_POST) )
+        {
+            $this->load->model("MVideos");
+            $status = $this->MVideos->addVideos($dat);
+        }
+
+        if ($status==1)
+        {
+            $data['status'] = "New Videos Added Successfully.";
+            $data['statusCode'] = 1;
+        }
+        else
+        {
+            $data['status'] = "Error occurred while adding new news.";
+            $data['statusCode'] = 0;
+        }
+        $this->View($data);
+    }
+
+    //Edit news dat in database
+    public function EditVideos()
+    {
+
+        //Load languages and Default Language
+        $languages = $this->MUtils->getLanguages();
+        $data['Languages'] = $languages;
+        $data['defaultLang'] = $this->MUtils->getDefaultLanguage();
+
+        $data['smallImage'] = $this->MUtils->doUploadWithCropping('smallFile', 70, 92);
+        //$data['largeImage'] = $this->MUtils->doUpload('largeFile', 0, 0, false);
+        //$data['sliderImage'] = $this->MUtils->doUpload('sliderFile', 1920, 530, true);
+
+        $dat['id'] = $this->input->post('id');
+        $dat['title'] = $this->input->post('title');
+        $dat['link'] = $this->input->post('link');
+        $dat['status'] = $this->input->post('status');
+
+
+        $data['date'] = $this->input->post("date");
+
+        $status = "";
+        $defaultCode = $languages[$data['defaultLang']]->code;
+        if ( isset($_POST ) )
+        {
+            $this->load->model("MVideos");
+            $status = $this->MVideos->editVideos($dat);
+        }
+
+
+        if ($status==1)
+        {
+            $data['status'] = "Videos Updated Successfully.";
+            $data['statusCode'] = 1;
+        }
+        else
+        {
+            $data['status'] = "Error occurred while adding news.";
+            $data['statusCode'] = 0;
+        }
+
+        $this->View($data);
+    }
+
+
+    public function Delete()
+    {
+        $id = (int)$this->input->get('id');
+
+        $this->load->model("MVideos");
+        $status = $this->MVideos->deleteVideos($id);
+
+        if ($status)
+            $this->MUtils->setSuccess("Record Deleted Successfully");
+        else
+            $this->MUtils->setError("Error occurred while deleting record");
+
+        echo $this->MUtils->getStatus();
+    }
+
+
 
 }
